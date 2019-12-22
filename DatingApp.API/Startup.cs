01a32441c35fbea245331c5b253c72ce88dd6109
Chetan jassal <jassal.chetan@yahoo.co.in>
2019-12-23 03:38:17 +0530

    Coorected

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -45,7 +45,7 @@ namespace DatingApp.API
                         ValidateIssuer = false,
                         ValidateAudience = false
 
-                }
+                };
                 });
         }
 
